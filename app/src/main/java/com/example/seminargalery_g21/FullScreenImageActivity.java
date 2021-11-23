8b64a614d18d6f5d88904a240ebe2f84d5d98a29
t4n-du4ng <dungnguyenq1w2@gmail.com>
2021-11-23 21:07:53 +0700

    hide status bar in FullScreenImageActivity

--- app/src/main/java/com/example/seminargalery_g21/FullScreenImageActivity.java
+++ app/src/main/java/com/example/seminargalery_g21/FullScreenImageActivity.java
@@ -1,8 +1,11 @@
 package com.example.seminargalery_g21;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
@@ -19,6 +22,9 @@ public class FullScreenImageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_full_screen_image);
 
         imgView = (ImageView) findViewById(R.id.image_solo);
