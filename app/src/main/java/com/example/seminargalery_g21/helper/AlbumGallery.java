fb496c56e4d4e3eec10a4909bf70021b68568021
TLCRATEAM <64020797+TLCRATEAM@users.noreply.github.com>
2021-12-07 21:42:39 +0700

    Update AlbumGallery.java

--- app/src/main/java/com/example/seminargalery_g21/helper/AlbumGallery.java
+++ app/src/main/java/com/example/seminargalery_g21/helper/AlbumGallery.java
@@ -5,22 +5,29 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.util.Log;
-
+import com.example.seminargalery_g21.R;
 import java.util.ArrayList;
 
 public class AlbumGallery {
     public static ArrayList<Album> listOfAlbum(Context context) {
         ArrayList <Album> albums = new ArrayList<>();
         ArrayList <String> albumsNames = new ArrayList<>();
+        
+        // Add Favorites and Recycle Bin Album
+        String imageUri1, imageUri2;
+        imageUri1 = Uri.parse("android.resource://" + R.class.getPackage().getName() + "/" + R.drawable.ic_favorite_folder).toString();
+        imageUri2 = Uri.parse("android.resource://" + R.class.getPackage().getName() + "/" + R.drawable.ic_bin_folder).toString();
+        albums.add(new Album("Favorites", imageUri1, new ArrayList<>()));
+        albums.add(new Album("Recycle Bin", imageUri2, new ArrayList<>()));
+        
         Uri uri;
         Cursor cursor;
         int column_index_data, column_index_folder_name;
-        ArrayList<String> listOfAllImages = new ArrayList<>();
         String absolutePathOfImages;
         uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
 
         String[] projection = {MediaStore.MediaColumns.DATA,
-                MediaStore.Images.Media.BUCKET_DISPLAY_NAME};
+                               MediaStore.Images.Media.BUCKET_DISPLAY_NAME};
 
         String orderBy = MediaStore.Video.Media.DATE_TAKEN;
         cursor = context.getContentResolver().query(uri, projection, null,null, orderBy+" DESC");
@@ -28,7 +35,6 @@ public class AlbumGallery {
         column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
 
         //get folder name
-//        column_index_folder_name = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
         column_index_folder_name = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
         String bucketName ;
 
@@ -44,23 +50,19 @@ public class AlbumGallery {
                 for ( Album album : albums ) {
                     if ( album.getAlbumName().equals( bucketName ) ) {
                         album.getAlbumPhotosUri().add( image );
-                        Log.i( "DeviceImageManager", "A photo was added to album => " + bucketName );
                         break;
                     }
                 }
             } else {
                 Album album = new Album(null, null, null);
-                Log.i( "DeviceImageManager", "A new album was created => " + bucketName );
                 album.setAlbumName( bucketName );
                 album.setAlbumImage( image.getPhotoUri() );
                 album.getAlbumPhotosUri().add( image );
-                Log.i( "DeviceImageManager", "A photo was added to album => " + bucketName );
 
                 albums.add( album );
                 albumsNames.add( bucketName );
             }
 
-//            listOfAllImages.add(absolutePathOfImages);
         }
         return albums;
     }
