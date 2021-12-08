54bb9dd97980be2ed6977c3712c86c6ecae5a3e2
Khai <khailqd81@gmail.com>
2021-12-08 13:11:04 +0700

    Connect to FullScreenImage activity (K)

--- app/src/main/java/com/example/seminargalery_g21/ImageInfoActivity.java
+++ app/src/main/java/com/example/seminargalery_g21/ImageInfoActivity.java
@@ -1,15 +1,15 @@
 package com.example.seminargalery_g21;
 
-import android.app.Activity;
+
 import android.content.Intent;
-import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.ParcelFileDescriptor;
-import android.provider.OpenableColumns;
+
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,134 +19,179 @@ import java.io.File;
 import java.io.FileDescriptor;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.text.ParseException;
+import java.util.Date;
+import java.text.DateFormat;
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 public class ImageInfoActivity extends AppCompatActivity {
-    Uri targetUri = null;
-    String filepath;
-    String size;
-    File file;
-    TextView name;
-    TextView fileSize;
-    TextView dateTime;
-    TextView height;
-    TextView width;
-    TextView device;
+
+    TextView txtName;
+    TextView txtSize;
+    TextView txtDateTime;
+    TextView txtHeight;
+    TextView txtWidth;
+    TextView txtDevice;
+    TextView txtDeviceTitle;
     TextView txtFilepath;
-    Button chooseBtn;
+    String size;
+    File imageFile;
+    ImageButton ic_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_info);
-        chooseBtn = (Button) findViewById(R.id.choose_btn);
-//        chooseBtn.setOnClickListener(chooseButtonOnClickListener);
-        name = (TextView) findViewById(R.id.name);
-        dateTime = (TextView) findViewById(R.id.date_time);
-        height = (TextView) findViewById(R.id.height);
-        width = (TextView) findViewById(R.id.width);
-        device = (TextView) findViewById(R.id.device);
-        fileSize = (TextView) findViewById(R.id.size);
-        txtFilepath = (TextView) findViewById(R.id.filepath);
+        getSupportActionBar().hide();
 
-        chooseBtn.setOnClickListener(new View.OnClickListener() {
+        // Lấy dữ liệu từ FullScreenImage
+        Intent intent = getIntent();
+        String path = intent.getStringExtra("path");
+        //
+        txtName = (TextView) findViewById(R.id.name);
+        txtDateTime = (TextView) findViewById(R.id.date_time);
+        txtHeight = (TextView) findViewById(R.id.height);
+        txtWidth = (TextView) findViewById(R.id.width);
+        txtDevice = (TextView) findViewById(R.id.device);
+        txtSize = (TextView) findViewById(R.id.size);
+        txtFilepath = (TextView) findViewById(R.id.filepath);
+        txtDeviceTitle = (TextView) findViewById(R.id.device_title) ;
+        // Mũi tên quay lại
+        ic_back = (ImageButton) findViewById(R.id.ic_back);
+        ic_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_OPEN_DOCUMENT);
-                intent.addCategory(Intent.CATEGORY_OPENABLE);
-                intent.setType("image/*");
-                startActivityForResult(intent, 1);
+                finish();
             }
         });
-    }
 
-//    View.OnClickListener chooseButtonOnClickListener =
-//            new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//
-//                    Intent intent = new Intent();
-//                    intent.setAction(Intent.ACTION_OPEN_DOCUMENT);
-//                    intent.addCategory(Intent.CATEGORY_OPENABLE);
-//                    intent.setType("image/*");
-//                    startActivityForResult(intent, 1);
-//                }
-//            };
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == Activity.RESULT_OK) {
-            Uri dataUri = data.getData();
-            targetUri = dataUri;
-            showExif(targetUri);
+        if (path != null) {
+            showExif(path);
+        } else {
+            Toast.makeText(getApplicationContext(), "Can't get Image Path.", Toast.LENGTH_LONG).show();
+            finish();
         }
     }
 
-    // Tách tên image từ filepath
-    private String setName(String filepath) {
+    // Tách tên ảnh từ filepath
+    private String getName(String filepath) {
         int pos = filepath.lastIndexOf("/");
         String title = filepath;
-
         if (-1 != pos) {
             title = filepath.substring(pos + 1);
         }
         return title;
     }
 
-    void showExif(Uri photoUri) {
-        if (photoUri != null) {
-
-            ParcelFileDescriptor parcelFileDescriptor = null;
-            String[] split = photoUri.getPath().split(":");
-            file = new File(photoUri.getPath());
-            filepath = split[1];
-            //How to retrieve info of file https://developer.android.com/training/secure-file-sharing/retrieve-info#java
-            Cursor returnCursor =
-                    getContentResolver().query(photoUri, null, null, null, null);
-            returnCursor.moveToFirst();
-            int sizeIndex = returnCursor.getColumnIndex(OpenableColumns.SIZE);
-            double sizeMB = returnCursor.getDouble(sizeIndex);
-            sizeMB = sizeMB / (1024 * 1024);
-            size = new DecimalFormat("#.0#").format(sizeMB);
-            /*
-            How to convert the Uri to FileDescriptor, refer to the example in the document:
-            https://developer.android.com/guide/topics/providers/document-provider.html
-             */
-            try {
-                parcelFileDescriptor = getContentResolver().openFileDescriptor(photoUri, "r");
-                FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
-                ExifInterface exifInterface = new ExifInterface(fileDescriptor);
-                name.setText("Tên ảnh: " + setName(filepath));
-                txtFilepath.setText("Vị trí: " + filepath);
-                fileSize.setText("Dung lượng: " + size + "MB");
-                height.setText("Độ dài: " + exifInterface.getAttribute(ExifInterface.TAG_IMAGE_LENGTH) + "px");
-                width.setText("Độ rộng: " + exifInterface.getAttribute(ExifInterface.TAG_IMAGE_WIDTH) + "px");
-                dateTime.setText("Ngày giờ chụp: " + exifInterface.getAttribute(ExifInterface.TAG_DATETIME));
-                device.setText("Thiết bị chụp: " + exifInterface.getAttribute(ExifInterface.TAG_MAKE) + " " + exifInterface.getAttribute(ExifInterface.TAG_MODEL));
-                parcelFileDescriptor.close();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-                Toast.makeText(getApplicationContext(),
-                        "Something wrong:\n" + e.toString(),
-                        Toast.LENGTH_LONG).show();
-            } catch (IOException e) {
-                e.printStackTrace();
-                Toast.makeText(getApplicationContext(),
-                        "Something wrong:\n" + e.toString(),
-                        Toast.LENGTH_LONG).show();
-            }
-
-            String strPhotoPath = photoUri.getPath();
-
+    // Tính kích thước ảnh
+    private String getFileSize(File file) {
+        String res;
+        double sizeKB = Double.parseDouble(String.valueOf(file.length() * 1.0 / 1024));
+        if (sizeKB > 1024) {
+            double sizeMB = sizeKB / 1024;
+            res = new DecimalFormat("#.0#").format(sizeMB) + " MB";
         } else {
-            Toast.makeText(getApplicationContext(),
-                    "Image Uri is null",
-                    Toast.LENGTH_LONG).show();
+            res = new DecimalFormat("#.0#").format(sizeKB) + " KB";
         }
+        return res;
     }
 
-    ;
+    // Định dạng thời gian
+    private String formatDate(String exifDateTime, File file) {
+        String res = "01/01/2001";
+        if (exifDateTime == null) {
+            Date lastModDate = new Date(file.lastModified());
+            res = DateFormat.getInstance().format(lastModDate);
+        } else {
+            try {
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss", Locale.US);
+                Date newDate = simpleDateFormat.parse(exifDateTime);
+                res = DateFormat.getInstance().format(newDate);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+        return res;
+    }
 
+    // Định dạng lại vị trí ảnh
+    private String formatFilePath(String filePath) {
+        String res = "/Bộ nhớ trong/";
+        String defaultPath = "/storage/emulated/0/";
+        int pos = filePath.indexOf(defaultPath);
+        if (pos != -1) {
+            int lastDash = filePath.lastIndexOf("/");
+            String subStr = filePath.substring(defaultPath.length(), lastDash);
+            res = res + subStr;
+        } else {
+            res = filePath;
+        }
+        return res;
+    }
+
+    // Hàm hiển thị các thông tin của bức ảnh
+    // tham khảo từ: http://android-er.blogspot.com/2017/04/read-exif-tag-of-jpg-using.html
+    public void showExif(String path) {
+        imageFile = new File(path);
+        Uri imageUri = (Uri) Uri.fromFile(imageFile);
+        size = getFileSize(imageFile);
+
+        try {
+            ParcelFileDescriptor parcelFileDescriptor = getContentResolver().openFileDescriptor(imageUri, "r");
+            FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+            ExifInterface exifInterface = new ExifInterface(fileDescriptor);
+
+            // Định dạng lại các thông tin trước khi hiện ra màn hình
+            String filePath = formatFilePath(path);
+            String dateTime = (String) exifInterface.getAttribute(ExifInterface.TAG_DATETIME);
+            dateTime = formatDate(dateTime, imageFile);
+            String make = exifInterface.getAttribute(ExifInterface.TAG_MAKE);
+            String model = exifInterface.getAttribute(ExifInterface.TAG_MODEL);
+            int height = Integer.parseInt(exifInterface.getAttribute(ExifInterface.TAG_IMAGE_LENGTH));
+            int width = Integer.parseInt(exifInterface.getAttribute(ExifInterface.TAG_IMAGE_WIDTH));
+            // Lấy thông tin chiều cao, chiều rộng, link tham khảo https://stackoverflow.com/questions/16440863/can-i-get-image-file-width-and-height-from-uri-in-android/16440953
+            if (height <= 0 || width <= 0) {
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inJustDecodeBounds = true;
+                BitmapFactory.decodeFile(path, options);
+                width = options.outWidth;
+                height = options.outHeight;
+            }
+            //
+//            txtName.setText("Tên ảnh: " + getName(path));
+//            txtFilepath.setText("Vị trí: " + filePath);
+//            txtSize.setText("Dung lượng: " + size);
+//            txtHeight.setText("Chiều cao: " + Integer.toString(height) + "px");
+//            txtWidth.setText("Chiều rộng: " + Integer.toString(width) + "px");
+//            txtDateTime.setText("Ngày giờ chụp: " + dateTime);
+//            if (make != null && model != null) {
+//                txtDevice.setText("Thiết bị chụp: " + make + " " + model);
+//            } else {
+//                txtDevice.setVisibility(View.INVISIBLE);
+//            }
+            txtName.setText( getName(path));
+            txtFilepath.setText(filePath);
+            txtSize.setText(size);
+            txtHeight.setText(Integer.toString(height) + "px");
+            txtWidth.setText(Integer.toString(width) + "px");
+            txtDateTime.setText(dateTime);
+            if (make != null && model != null) {
+                txtDevice.setText(make + " " + model);
+            } else {
+                txtDevice.setVisibility(View.INVISIBLE);
+                txtDeviceTitle.setVisibility(View.INVISIBLE);
+            }
+            parcelFileDescriptor.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "Error FileNotFoundException:\n" + e.toString(), Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "Error IOException:\n" + e.toString(), Toast.LENGTH_LONG).show();
+        }
+
+    }
 }
